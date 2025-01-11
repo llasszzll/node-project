31019ab69395e32cf2f83cdb5267a5a90494b44b
llasszzll@gmail.com <llasszzll@gmail.com>
2025-01-11 15:02:26 +0200

    First commit

---
+++ app.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+// Create a server and Packages
+const express = require('express');
+const bodyParser = require('body-parser');
+const expressHbs = require('express-handlebars');
+
+const app = express();
+
+// hbs = Handlebars
+app.engine('hbs', expressHbs())
+app.set('view engine', 'hbs');
+app.set('views', 'views')
+
+const adminData = require('./routes/admin')
+const shopRoutes = require('./routes/shop')
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(express.static(path.join(__dirname, 'public')))  // serve static CSS
+
+app.use('/admin', adminData.routes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+    res.status(404).render('404', { pageTitle: 'Page Not Found' });
+});
+
+app.listen(3000);
