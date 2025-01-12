898fdc50fcf1c4e6720195c939620d6a18be565d
llasszzll@gmail.com <llasszzll@gmail.com>
2025-01-12 19:12:45 +0200

    Added Views and Controllers

---
+++ models/product.js
@@ -0,0 +1,41 @@
+const fs = require('fs');
+const path = require('path');
+
+
+// "p" is defined for global scope on this file
+const p = path.join(
+    path.dirname(require.main.filename),
+    'data',
+    'products.json'
+);
+
+// Function over here
+// Understand the THIS keyword
+const getProductsFromFile = cb => {
+    fs.readFile(p, (err, fileContent) => {
+        if (err) {
+            cb([]);
+        } else {
+            cb(JSON.parse(fileContent));
+        };
+    });
+};
+
+module.exports = class Product {
+    constructor(t) {
+        this.title = t;
+    }
+
+    save() {
+        getProductsFromFile(products => {
+            products.push(this);
+            fs.writeFile(p, JSON.stringify(products), (err) => {
+                console.log(err);
+            });
+        });
+    };
+
+    static fetchAll(cb) {
+        getProductsFromFile(cb);
+    };
+};
