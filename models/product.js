9aaad01932a523bf641e512751b969f76943ee85
llasszzll@gmail.com <llasszzll@gmail.com>
2025-01-13 14:52:15 +0200

    Added Product Detail Page

--- models/product.js
+++ models/product.js
@@ -22,11 +22,15 @@ const getProductsFromFile = cb => {
 };
 
 module.exports = class Product {
-    constructor(t) {
-        this.title = t;
+    constructor(title, imageUrl, description, price) {
+        this.title = title;
+        this.imageUrl = imageUrl;
+        this.description = description;
+        this.price = price;
     }
 
     save() {
+        this.id = Math.random().toString();
         getProductsFromFile(products => {
             products.push(this);
             fs.writeFile(p, JSON.stringify(products), (err) => {
@@ -38,4 +42,11 @@ module.exports = class Product {
     static fetchAll(cb) {
         getProductsFromFile(cb);
     };
+
+    static findById(id, cb) {
+        getProductsFromFile(products => {
+            const product = products.find(p => p.id === id);
+            cb(product);
+        });
+    }
 };
